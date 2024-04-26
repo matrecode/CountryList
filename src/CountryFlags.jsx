6bf2a604bb3368997d496e760b538e919b36652a
matrecode <akshaymatrecode@gmail.com>
2024-04-26 17:49:29 +0530

    optimized search functionality with debouncing

--- src/CountryFlags.jsx
+++ src/CountryFlags.jsx
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from "react";
 import styles from "./CountryFlags.module.css";
 
+const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+};
+
 const CountryFlags = () => {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState("");
+  const debounceSearch = useDebounce(search, 300);
   function fetchCountry() {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
@@ -15,10 +31,18 @@ const CountryFlags = () => {
     fetchCountry();
   }, []);
 
-  const filteredCountries = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(search.toLowerCase())
-  );
-
+  useEffect(() => {
+    // Filter countries only when debounced search value changes
+    if (debounceSearch !== "") {
+      const filteredCountries = countries.filter((country) =>
+        country.name.common.toLowerCase().includes(debounceSearch.toLowerCase())
+      );
+      setCountries(filteredCountries);
+    } else {
+      // Reset countries to show all when search input is empty
+      fetchCountry();
+    }
+  }, [debounceSearch]);
   // console.log(filteredCountries);
 
   const handleSearchChange = (e) => {
@@ -28,19 +52,12 @@ const CountryFlags = () => {
     <>
       <input type="text" value={search} onChange={handleSearchChange} />
       <div className={styles.container}>
-        {search === ""
-          ? countries.map((country) => (
-              <div className={styles.countryCard} key={country.name.common}>
-                <img src={country.flags.png} alt={country.name.common} />
-                <p>{country.name.common}</p>
-              </div>
-            ))
-          : filteredCountries.map((country) => (
-              <div className={styles.countryCard} key={country.name.common}>
-                <img src={country.flags.png} alt={country.name.common} />
-                <p>{country.name.common}</p>
-              </div>
-            ))}
+        {countries.map((country) => (
+          <div className={styles.countryCard} key={country.name.common}>
+            <img src={country.flags.png} alt={country.name.common} />
+            <p>{country.name.common}</p>
+          </div>
+        ))}
       </div>
     </>
   );

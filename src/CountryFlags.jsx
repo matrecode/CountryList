51ecf1ba045afcdbf48b2f4bd3b9e70b7eef7396
matrecode <akshaymatrecode@gmail.com>
2024-04-26 16:51:06 +0530

    Added search functionality

--- src/CountryFlags.jsx
+++ src/CountryFlags.jsx
@@ -2,27 +2,44 @@ import React, { useState, useEffect } from "react";
 import styles from "./CountryFlags.module.css";
 
 const CountryFlags = () => {
-  const [country, setCountry] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
   function fetchCountry() {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => setCountry(data))
+      .then((data) => setCountries(data))
       .catch((error) => console.log("APi is not working", error));
   }
 
   useEffect(() => {
     fetchCountry();
   }, []);
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <>
       <div className={styles.container}>
+        <input type="text" value={search} onChange={handleSearchChange} />
         <div className={styles.box}>
-          {country.map((country) => (
-            <div className={styles.card} key={country.name.common}>
-              <img src={country.flags.png} alt={country.name.common} />
-              <p>{country.name.common}</p>
-            </div>
-          ))}
+          {search === ""
+            ? countries.map((country) => (
+                <div className={styles.countryCard} key={country.name.common}>
+                  <img src={country.flags.png} alt={country.name.common} />
+                  <p>{country.name.common}</p>
+                </div>
+              ))
+            : filteredCountries.map((country) => (
+                <div className={styles.countryCard} key={country.name.common}>
+                  <img src={country.flags.png} alt={country.name.common} />
+                  <p>{country.name.common}</p>
+                </div>
+              ))}
         </div>
       </div>
     </>
